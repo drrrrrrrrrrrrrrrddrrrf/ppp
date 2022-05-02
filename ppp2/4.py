ff6d8f114bc8cd9b939f5c12df5c5a02880cbda2
nhgjk <ricard.garcia.2197@lacetania.cat>
2022-05-02 17:28:13 +0200

    :D

--- ppp2/4.py
+++ ppp2/4.py
@@ -1,8 +1,7 @@
 a =[1,2,3,4,5,6,7,8,9]
 n=int(input("Num: "))
-if n in a:
+while n not in a:
+    print ("No és un número del 1 al 9")
     n=int(input("Num: "))
-    print("n")
 else:
-    print("no és un número del 1 al 9")
-    n=int(input("Num: "))
+    print (n)
