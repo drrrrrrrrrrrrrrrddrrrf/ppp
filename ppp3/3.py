c20fff9d4f16caa90c10d12e607395e1f1558da1
siuuuu <siuuuuuu@siuuuuuu.com>
2022-05-03 20:25:09 +0200

    :D

--- ppp3/3.py
+++ ppp3/3.py
@@ -1,7 +1,4 @@
-c = input("Num: ")
-newstr = c
-vowels = ('a', 'e', 'i', 'o', 'u')
-for x in c.lower():
-    if x in vowels:
-        newstr = newstr.replace(x,"")
-        print (newstr)
+a = input("Frase: ")
+for b in ['b', 'c', 'd', 'f', 'g', 'h','j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']:
+    if b in a:
+        print(b)
