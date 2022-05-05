0ede8902e6c5c174cb78b7a8b3b66a864a3034be
alalalal <ricard.garcia.2197@lacetania.cat>
2022-05-05 20:28:05 +0200

    :D

---
+++ ppp3/7.py
@@ -0,0 +1,7 @@
+n=int(input("Num: "))
+for i in range(n):
+    z=int(input("Num: "))
+    if z%2 == 0:
+        print(z,"Es parell ")
+    else:
+        print(z,"Es inparell")
---
+++ ppp3/8.py
@@ -0,0 +1,5 @@
+n=int(input("Num: "))
+for i in range(n):
+    z=int(input("Num2: "))
+    if z < n:
+        print("El Numero ",z, " es mes petit que ",n)
