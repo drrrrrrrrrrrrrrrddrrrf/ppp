3a2b409344b6f8f810fb47fcfbc3396453ba86b2
alalalal <ricard.garcia.2197@lacetania.cat>
2022-05-18 18:33:39 +0200

    :D

--- ppp4/4.py
+++ ppp4/4.py
@@ -1,10 +1,8 @@
 n = input("Frase: ")
 z = n.split()
-r = []
 for i in range (len(z)):
-    r.append(len(z[i]))
-    m = r[0]
+    m = z[0]
 for h in range(len(z)):
-    if(r[h]>m):
-        m = r[h]
-        print(z[0+h])
+    if(z[h]>m):
+        m = z[h]
+print(m)
