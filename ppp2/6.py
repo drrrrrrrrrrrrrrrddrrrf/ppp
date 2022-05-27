7c72e73f5534bfea311bfd08978dd1f3b95427e8
nhgjk <ricard.garcia.2197@lacetania.cat>
2022-05-27 03:47:21 +0200

    :D

--- ppp2/6.py
+++ ppp2/6.py
@@ -1,7 +1,10 @@
-n=int(input("Num: "))
-while n not in ["a"]:
-    print("6 *",n, "es",n*6)
-    n+=1
-    r=input("Desitja sortir (S/N)? ")
-    if r in ["S","s"]:
+while 0==0:
+    x=int(input("Num: "))
+    print("6 *",x, "es",x*6)
+    n=1
+    for i in range(10):
+        print("6 *",n, "es",n*6)
+        n+=1
+    l=str(input("Desitja sortir (S/N): "))
+    if l.lower() in {"s"}:
         break
